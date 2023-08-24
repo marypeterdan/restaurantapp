6399918bd0897ffaab4c1ae303da81be235cf84b
Mary.rani <mary.rani@stackroute.in>
2023-08-24 12:49:55 +0530

    Restaurant

---
+++ src/main/java/com/stackroute/restaurantapp/MyController.java
@@ -0,0 +1,17 @@
+package com.stackroute.restaurantapp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("home")
+	public ResponseEntity getHome()
+	{
+		return new ResponseEntity("hello",HttpStatus.OK);
+	}
+
+}
---
+++ src/main/java/com/stackroute/restaurantapp/RestaurantappApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.restaurantapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantappApplication.class, args);
+		System.out.println("Hai all welcome");
+	}
+
+}
